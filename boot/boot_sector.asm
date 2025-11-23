0118b2a3cd90c8b6534c07d13939bc8021298a5d
Alex Schmith <alexanderschmith@protonmail.com>
2025-11-23 09:03:31 -0500

    seperated files

--- boot/boot_sector.asm
+++ boot/boot_sector.asm
@@ -11,30 +11,20 @@ start:
     ; Setup stack to 0x7C00
     xor ax, ax
     mov ss, ax
-    mov sp, 0x7C00
+    mov sp, 0x8000
+    mov bp, sp
 
     cld
 
 print_startup:
     mov si, startup_string
-
-print_string:
-    lodsb 
-    or al, al
-    jz loop
-
-    call print_char
-
-    jmp print_loop
+    call print_string
 
 loop:
     jmp loop
 
-print_char:
-    mov ah, 0x0e
-    int 0x10
 
-    ret
+%include "common.asm"
 
 startup_string db "[*] Starting up zigzag kernel bootloader", 10, 0
 
---
+++ boot/common.asm
@@ -0,0 +1,22 @@
+[org 0x7C00]
+bits 16
+
+print_string:
+    cld
+
+print_string_loop:
+    lodsb 
+    or al, al
+    jz loop
+
+    call print_char
+
+    jmp print_string_loop
+
+print_char:
+    mov ah, 0x0e
+    int 0x10
+
+    ret
+
+
